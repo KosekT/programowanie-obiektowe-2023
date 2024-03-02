ea38b54102a97aeffec865e5b14221b689318acf
Tomasz Kosek <stomasz1999@gmail.com>
2024-03-02 10:29:22 +0100

    feat - client class

--- src/z9_chargers/chargin_station.py
+++ src/z9_chargers/chargin_station.py
@@ -3,3 +3,9 @@ class Car:
         self.vin = vin
         self.total_charged_kwh = total_charged_kwh
         self.max_current_kw = max_current_kw
+
+class ClientAccount:
+    def __init__(self, id: int, name: str, funds: float):
+        self.id = id
+        self.name = name
+        self.funds = funds
